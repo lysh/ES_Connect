405c512ea1ba67b9bf73cc44a037749e6b1d40e3
lysh <lysh@mail.ustc.edu.cn>
2018-04-08 19:02:33 +0800

    修改函数方法

--- es_connect_test.py
+++ es_connect_test.py
@@ -1,228 +1,174 @@
-#coding:utf8
+#coding=utf8
+
 import os
-import time
 from os import walk
-import csvop
+
+import time
 from datetime import datetime
+import json
+import csvop
+
 from elasticsearch import Elasticsearch
 from elasticsearch.helpers import bulk
 
 
-class ElasticObj:
-    def __init__(self, index_name,index_type,ip ="127.0.0.1"):
-        '''
 
+class ElasticObj:
+    def __init__(self, ip ="127.0.0.1"):
+        '''
         :param index_name: 索引名称
         :param index_type: 索引类型
         '''
-        self.index_name =index_name
-        self.index_type = index_type
         # 无用户名密码状态
-        #self.es = Elasticsearch([ip])
+        self.es = Elasticsearch([ip])
         #用户名密码状态
-        self.es = Elasticsearch([ip],http_auth=('elastic', 'password'),port=9200)
+        #self.es = Elasticsearch([ip],http_auth=('elastic', 'password'),port=9200)
 
-    def create_index(self,index_name="ott",index_type="ott_type"):
+
+    def check(self):
         '''
-        创建索引,创建索引名称为ott，类型为ott_type的索引
-        :param ex: Elasticsearch对象
+        输出当前系统的ES信息
         :return:
         '''
-        #创建映射
-        _index_mappings = {
-            "mappings": {
-                self.index_type: {
-                    "properties": {
-                        "title": {
-                            "type": "text",
-                            "index": True,
-                            "analyzer": "ik_max_word",
-                            "search_analyzer": "ik_max_word"
-                        },
-                        "date": {
-                            "type": "text",
-                            "index": True
-                        },
-                        "keyword": {
-                            "type": "string",
-                            "index": "not_analyzed"
-                        },
-                        "source": {
-                            "type": "string",
-                            "index": "not_analyzed"
-                        },
-                        "link": {
-                            "type": "string",
-                            "index": "not_analyzed"
-                        }
-                    }
-                }
-
-            }
-        }
-        if self.es.indices.exists(index=self.index_name) is not True:
-            res = self.es.indices.create(index=self.index_name, body=_index_mappings)
-            print res
+        return self.es.info()
 
 
-    def IndexData(self):
-        es = Elasticsearch()
-        csvdir = 'D:/work/ElasticSearch/exportExcels'
-        filenamelist = []
-        for (dirpath, dirnames, filenames) in walk(csvdir):
-            filenamelist.extend(filenames)
-            break
-        total = 0
-        for file in filenamelist:
-            csvfile = csvdir + '/' + file
-            self.Index_Data_FromCSV(csvfile)
-            total += 1
-            print total
-            time.sleep(10)
 
-    def Index_Data_FromCSV(self,csvfile):
+    def create_index(self, index_name, index_mappings):
+        '''
+        创建索引,创建索引名称为index_name，类型为index_type的索引
+        :param index_mappings: 创建索引的映射
+        :return:
+        '''
+        if self.es.indices.exists(index=index_name) is not True:
+            _created = self.es.indices.create(index=index_name, body=index_mappings)
+            print(_created)
+            return _created
+
+
+    def insert_one_document(self, index_name, index_type, body, id=None):
+        '''
+        插入一条数据body到指定的index、指定的type下;可指定Id,若不指定,ES会自动生成
+        :param index_name: 待插入的index值
+        :param index_type: 待插入的type值
+        :param body: 待插入的数据 -> dict型
+        :param id: 自定义Id值
+        :return:
+        '''
+        _inserted = self.es.index(index=index_name, doc_type=index_type, body=body, id=id)
+        print(_inserted['result'])
+        return _inserted
+
+
+    def index_data_fromCSV(self, index_name, index_type, csvfile):
         '''
         从CSV文件中读取数据，并存储到es中
         :param csvfile: csv文件，包括完整路径
         :return:
         '''
-        list = csvop.read_csv(csvfile)
+        data_list = csvop.read_csv(csvfile)
         index = 0
         doc = {}
-        for item in list:
-            if index > 1:#第一行是标题
-                doc['title'] = item[0]
-                doc['link'] = item[1]
-                doc['date'] = item[2]
-                doc['source'] = item[3]
-                doc['keyword'] = item[4]
-                res = self.es.index(index=self.index_name, doc_type=self.index_type, body=doc)
-                #print(res['created'])
+        title = []
+        title_num = len(data_list[0])
+        for i in range(title_num):#第一行是标题
+            title.append(data_list[0][i])
+        for item in data_list:
+            if index >= 1:
+                for i in range(title_num):
+                    doc[title[i]] = item[i]
+                res = self.es.index(index=index_name, doc_type=index_type, body=doc)
+                print(res['result'])
             index += 1
-            print index
+            #print(index)
 
-    def Index_Data(self):
+    def insert_DataFrame(self, index_name, index_type, dataFrame):
         '''
-        数据存储到es
+        使用bulk方法批量插入接口;
+        bulk接口所要求的数据列表结构为:[{{optionType}: {Condition}}, {data}]
+        其中optionType可为index、delete、update
+        Condition可设置每条数据所对应的index值和type值
+        data为具体要插入/更新的单条数据
+        :param index_name: 默认插入的index值
+        :param index_type: 默认插入的type值
+        :param dataFrame: 待插入数据集
         :return:
         '''
-        list = [
-            {   "date": "2017-09-13",
-                "source": "慧聪网",
-                "link": "http://info.broadcast.hc360.com/2017/09/130859749974.shtml",
-                "keyword": "电视",
-                "title": "付费 电视 行业面临的转型和挑战"
-             },
-            {   "date": "2017-09-13",
-                "source": "中国文明网",
-                "link": "http://www.wenming.cn/xj_pd/yw/201709/t20170913_4421323.shtml",
-                "keyword": "电视",
-                "title": "电视 专题片《巡视利剑》广获好评：铁腕反腐凝聚党心民心"
-             }
-              ]
-        for item in list:
-            res = self.es.index(index=self.index_name, doc_type=self.index_type, body=item)
-            #print(res['created'])
+        index_name = index_name
+        index_type = index_type
+        dataList = dataFrame.to_dict(orient='records')
+        insertHeadInfoList = [{"index": {}} for i in range(len(dataList))]
+        temp = [dict] * (len(dataList) * 2)
+        temp[::2] = insertHeadInfoList
+        temp[1::2] = dataList
+        try:
+            return self.es.bulk(index=index_name, doc_type=index_type, body=temp)
+        except Exception, e:
+            return str(e)
 
-    def bulk_Index_Data(self):
-        '''
-        用bulk将批量数据存储到es
-        :return:
-        '''
-        list = [
-            {"date": "2017-09-13",
-             "source": "慧聪网",
-             "link": "http://info.broadcast.hc360.com/2017/09/130859749974.shtml",
-             "keyword": "电视",
-             "title": "付费 电视 行业面临的转型和挑战"
-             },
-            {"date": "2017-09-13",
-             "source": "中国文明网",
-             "link": "http://www.wenming.cn/xj_pd/yw/201709/t20170913_4421323.shtml",
-             "keyword": "电视",
-             "title": "电视 专题片《巡视利剑》广获好评：铁腕反腐凝聚党心民心"
-             },
-            {"date": "2017-09-13",
-             "source": "人民电视",
-             "link": "http://tv.people.com.cn/BIG5/n1/2017/0913/c67816-29533981.html",
-             "keyword": "电视",
-             "title": "中国第21批赴刚果（金）维和部隊启程--人民 电视 --人民网"
-             },
-            {"date": "2017-09-13",
-             "source": "站长之家",
-             "link": "http://www.chinaz.com/news/2017/0913/804263.shtml",
-             "keyword": "电视",
-             "title": "电视 盒子 哪个牌子好？ 吐血奉献三大选购秘笈"
-             }
-        ]
-        ACTIONS = []
-        i = 1
-        for line in list:
-            action = {
-                "_index": self.index_name,
-                "_type": self.index_type,
-                "_id": i, #_id 也可以默认生成，不赋值
-                "_source": {
-                    "date": line['date'],
-                    "source": line['source'].decode('utf8'),
-                    "link": line['link'],
-                    "keyword": line['keyword'].decode('utf8'),
-                    "title": line['title'].decode('utf8')}
-            }
-            i += 1
-            ACTIONS.append(action)
-            # 批量处理
-        success, _ = bulk(self.es, ACTIONS, index=self.index_name, raise_on_error=True)
-        print('Performed %d actions' % success)
 
-    def Delete_Index_Data(self,id):
+    def deleteDocById(self, index_name, index_type, id):
         '''
-        删除索引中的一条
+        删除指定index、type、id对应的数据
+        :param index_name:
+        :param index_type:
         :param id:
         :return:
         '''
-        res = self.es.delete(index=self.index_name, doc_type=self.index_type, id=id)
-        print res
+        return self.es.delete(index=index_name, doc_type=index_type, id=id)
 
-    def Get_Data_Id(self,id):
+    def deleteDocByQuery(self, index_name, query, doc_type=None):
+        '''
+        删除idnex下符合条件query的所有数据
+        :param index_name:
+        :param query: 满足DSL语法格式
+        :param doc_type:
+        :return:
+        '''
+        try:
+            res = self.es.delete_by_query(index=index_name, body=query, doc_type=doc_type)
+            return res
+        except Exception, e:
+            return str(e)
 
-        res = self.es.get(index=self.index_name, doc_type=self.index_type,id=id)
-        print(res['_source'])
-
-        print '------------------------------------------------------------------'
-        #
-        # # 输出查询到的结果
-        for hit in res['hits']['hits']:
+    def searchDoc(self, index_name=None, doc_type=None, body=None):
+        '''
+        查找index下所有符合条件的数据
+        :param index_name:
+        :param doc_type:
+        :param body: 筛选语句,符合DSL语法格式
+        :return:
+        '''
+        _searched = self.es.search(index=index_name, doc_type=doc_type, body=body)
+        #for hit in _searched['hits']['hits']:
             # print hit['_source']
-            print hit['_source']['date'],hit['_source']['source'],hit['_source']['link'],hit['_source']['keyword'],hit['_source']['title']
+        return _searched
 
-    def Get_Data_By_Body(self):
-        # doc = {'query': {'match_all': {}}}
-        doc = {
-            "query": {
-                "match": {
-                    "keyword": "电视"
-                }
-            }
-        }
-        _searched = self.es.search(index=self.index_name, doc_type=self.index_type, body=doc)
-
-        for hit in _searched['hits']['hits']:
+    def getDocById(self, index_name, doc_type, id):
+        '''
+        获取指定index_name、doc_type、id对应的数据
+        :param index_name:
+        :param doc_type:
+        :param id:
+        :return:
+        '''
+        _searched = self.es.get(index=index_name, doc_type=doc_type, id=id)
+        #for hit in _searched['hits']['hits']:
             # print hit['_source']
-            print hit['_source']['date'], hit['_source']['source'], hit['_source']['link'], hit['_source']['keyword'], \
-            hit['_source']['title']
+        return _searched
+
+
+    def updateDocById(self, index_name, doc_type, id, body=None):
+        '''
+        更新指定index_name、doc_type、id对应的数据
+        :param index_name:
+        :param doc_type:
+        :param id:
+        :param body: 待更新的值
+        :return:
+        '''
+        _updated = self.es.update(index=index_name, doc_type=doc_type, id=id, body=body)
+        return _updated
 
 
 
-
-obj =ElasticObj("ott","ott_type")
-# obj = ElasticObj("ott1", "ott_type1")
-
-# obj.create_index()
-obj.Index_Data()
-# obj.bulk_Index_Data()
-# obj.IndexData()
-# obj.Delete_Index_Data(1)
-# csvfile = 'D:/work/ElasticSearch/exportExcels/2017-08-31_info.csv'
-# obj.Index_Data_FromCSV(csvfile)
-# obj.GetData(es)
